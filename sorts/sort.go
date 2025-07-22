9fd296da67eb66322ee66808f0bb7c330b68c6b4
yerik.ussayev <yerik.ussayev@tele2.kz>
2025-07-22 17:01:48 +0500

    feat(sorts): add more methods

--- sorts/sort.go
+++ sorts/sort.go
@@ -1,6 +1,32 @@
 package sorts
 
+const (
+	Asc  = "ASC"
+	Desc = "DESC"
+)
+
 type Params struct {
 	Field string
 	Asc   bool
+}
+
+func (p Params) Empty() bool {
+	return p.Field == ""
+}
+
+func (p Params) Query() string {
+	var direction string
+	if p.Asc {
+		direction = Asc
+	} else {
+		direction = Desc
+	}
+	return p.Field + " " + direction
+}
+
+func (p Params) Direction() string {
+	if p.Asc {
+		return Asc
+	}
+	return Desc
 }
