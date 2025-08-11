6222a91443825f01b7d17a5c48c680178c31e8db
yerik.ussayev <yerik.ussayev@tele2.kz>
2025-08-11 17:33:03 +0500

    feat(sorts): add alias wrap for field

--- sorts/sort.go
+++ sorts/sort.go
@@ -1,5 +1,9 @@
 package sorts
 
+import (
+	"fmt"
+)
+
 const (
 	Asc  = "ASC"
 	Desc = "DESC"
@@ -14,14 +18,14 @@ func (p Params) Empty() bool {
 	return p.Field == ""
 }
 
-func (p Params) Query() string {
+func (p Params) Query(alias ...string) string {
 	var direction string
 	if p.Asc {
 		direction = Asc
 	} else {
 		direction = Desc
 	}
-	return p.Field + " " + direction
+	return wrapFieldWithAlias(p.Field, alias...) + " " + direction
 }
 
 func (p Params) Direction() string {
@@ -29,4 +33,13 @@ func (p Params) Direction() string {
 		return Asc
 	}
 	return Desc
+}
+
+func wrapFieldWithAlias(field string, alias ...string) string {
+	var setAlias string
+	if len(alias) > 0 {
+		setAlias = alias[0]
+	}
+
+	return fmt.Sprintf("%s%s", setAlias, field)
 }
